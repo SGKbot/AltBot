55b60df86ef65f285af8e85ce5721bc94cc62181
SGKespace <55636018+SGKespace@users.noreply.github.com>
2019-10-07 14:42:34 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,23 @@
-import telebot;                                                            #  Подключение модуля из стандартной библиотеки
+import telebot;                                                            
 bot = telebot.TeleBot('930977876:AAFpDgzP81IKXIULREWXIeWbxTxHGydHg6s'); 
-from telebot import types                                                  #  подключаем атрибут
+from telebot import types                                                  
 
-markup = types.ReplyKeyboardMarkup()                                       #  Создаем объект типа ReplyKeyboardMarkup()
+markup = types.ReplyKeyboardMarkup()                                       
 
 itembtnPrivet  = types.KeyboardButton('Привет')                            
 itembtnPoka    = types.KeyboardButton('Пока')
 itembtncProba  = types.KeyboardButton('#Проба')
 itembtndAmour  = types.KeyboardButton('Я тебя люблю')
 itembtneEmpty  = types.KeyboardButton('Empty')
-markup.row(itembtnPrivet, itembtnPoka, itembtncProba)                      #  добавляем построчно элементы
+markup.row(itembtnPrivet, itembtnPoka, itembtncProba)                      
 markup.row(itembtndAmour, itembtneEmpty)
 
-@bot.message_handler(commands=['start'])                                   #  декоратор
+@bot.message_handler(commands=['start'])                                   
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=markup)
 
-@bot.message_handler(content_types=['text'])                                #  декоратор
-def send_text(message):                                                     # Определим функцию
+@bot.message_handler(content_types=['text'])                               
+def send_text(message):                                                    
     if message.text.lower() == 'привет':
         bot.send_message(message.chat.id, 'Привет, мой создатель')
     elif message.text.lower() == 'пока':
