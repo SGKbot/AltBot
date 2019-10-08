0a9887cf522603302cf233dfc80cdac3f15b6828
SGKespace <sgkond@gmail.com>
2019-10-08 21:38:11 +0400

    1

--- bot.py
+++ bot.py
@@ -4,27 +4,27 @@ from telebot import types
 
 markup = types.ReplyKeyboardMarkup(True,True)                                       
 
-itembtnPrivet  = types.KeyboardButton('Привет')  #  Новости                          
-itembtnPoka    = types.KeyboardButton('Пока')    #  Дача
-itembtnСomb  = types.KeyboardButton('Объединить')
-itembtndAmour  = types.KeyboardButton('Я тебя люблю') #  Юмор
-itembtnSend  = types.KeyboardButton('Отправить')
-itembtnRead  = types.KeyboardButton('Читать далее...')
-itembtnIt  = types.KeyboardButton('#IT')
-itembtnBla  = types.KeyboardButton('#Мои рассуждения')
+itembtnPrivet = types.KeyboardButton('#Новости')
+itembtnPoka = types.KeyboardButton('Пока')    #  Дача
+itembtnСomb = types.KeyboardButton('Объединить')
+itembtndAmour = types.KeyboardButton('Я тебя люблю') #  Юмор
+itembtnSend = types.KeyboardButton('Отправить')
+itembtnRead = types.KeyboardButton('Читать далее...')
+itembtnIt = types.KeyboardButton('#IT')
+itembtnBla = types.KeyboardButton('#Мои рассуждения')
 
 
 markup.row(itembtnPrivet, itembtnIt, itembtnPoka, itembtnСomb)                      
-markup.row(itembtndAmour,itembtnBla , itembtnRead, itembtnSend)
+markup.row(itembtndAmour, itembtnBla, itembtnRead, itembtnSend)
 
 @bot.message_handler(commands=['start'])                                   
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])                               
-def send_text(message):                                                    
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
+def send_text(message):
+    if message.text.lower() == '#Новости':
+        bot.send_message(message.chat.id, 'Привет, мой создатель, давно пургу не копипастил')
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Прощай, создатель')
     elif message.text.lower() == 'я тебя люблю':
