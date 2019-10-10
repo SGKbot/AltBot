40791e4db75a84f2d73bf284a7215bd9dacde66a
SGKespace <sgkond@gmail.com>
2019-10-10 15:41:55 +0400

    from telebot import apihelper delete_message

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import telebot
 bot = telebot.TeleBot('930977876:AAFpDgzP81IKXIULREWXIeWbxTxHGydHg6s')
 from telebot import types                                                  
+from telebot import apihelper
 
 markup = types.ReplyKeyboardMarkup(True)
 
@@ -31,7 +32,7 @@ def send_text(message):
     elif message.text.lower() == 'прогресс':
         bot.send_message(message.chat.id, '#Проба(https://t.me/sgk_proba)')
     elif message.text.lower() == 'далее...':
-        bot.send_message(message.chat.id, '#Проба(https://t.me/sgk_proba)')
+        bot.delete_message(message.id)
     elif message.text.lower() == 'объединить':
         bot.send_message(message.chat.id, message.message_id)
     elif message.text.lower() == 'мнение':
